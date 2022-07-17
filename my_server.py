879c5ebaeab748fb6710b6a6f9d44a8028a44ae5
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-07-17 13:06:45 -0300

    trying to send a message to appinventor

--- my_server.py
+++ my_server.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 HOST = ""  # any connection allowed
 PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
@@ -13,4 +14,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             raw_data = conn.recv(1024)
             data = raw_data.decode('utf-8')
-            print(type(data))
+            print(f'{data} | {type(data)}')
+            s.sendto(b'received', addr)
+
+            #json_decoded = json.loads(raw_data)
+            #print(json_decoded)
+            
