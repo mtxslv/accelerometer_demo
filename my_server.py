379a3b6f10526c74f96090ad8145b327ddc82c56
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-07-18 13:01:46 -0300

    connection between app and server working

--- my_server.py
+++ my_server.py
@@ -1,22 +1,23 @@
 import socket
-import json
+
+# https://community.appinventor.mit.edu/t/gottext-not-triggered/34897/2
+# https://mundoprojetado.com.br/controlando-nodemcu-por-aplicativo-android/
+# https://community.appinventor.mit.edu/t/http-post-format-problem/11506/5
+
 
 HOST = ""  # any connection allowed
 PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
+confirmation = b"HTTP/1.1 200 OK\n\n"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    print('socket bound')
-    print('listening...')
-    s.listen()
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            raw_data = conn.recv(1024)
-            data = raw_data.decode('utf-8')
-            print(f'{data} | {type(data)}')
-            s.sendto(b'received', addr)
-
-            #json_decoded = json.loads(raw_data)
-            #print(json_decoded)
-            
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # criacao do socket TCP
+serverSocket.bind((HOST,PORT)) # bind do ip do servidor com a porta
+serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
+print ('Servidor TCP esperando conexoes na porta %d ...' % (PORT))
+while 1:
+    connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
+    sentence = connectionSocket.recv(16384) # recebe dados do cliente
+    sentence = sentence.decode('utf-8')
+    print(f'{sentence} | {type(sentence)}')
+    connectionSocket.send(confirmation) # confirmação de recebimento
+    connectionSocket.close() # encerra o socket com o cliente
+serverSocket.close() # encerra o socket do servidor
